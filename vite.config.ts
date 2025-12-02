0725a147a01528b39484d4c71294b664ae8e2d38
Stefano Mercadante <stefmercadante@gmail.com>
2025-12-02 09:43:17 +0100

    start pixi.js project

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.',
+  publicDir: 'public',
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
+});
